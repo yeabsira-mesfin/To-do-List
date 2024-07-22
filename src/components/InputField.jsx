9a8ca24e3b69e16b957efebde56d1da90550fb3c
yeabsira_mesfin <yabidesta@gmail.com>
2024-07-22 07:12:33 -0400

    Input Field

---
+++ src/components/InputField.jsx
@@ -0,0 +1,18 @@
+import {useState} from 'react'
+
+const InputField = () => {
+    const [inputF,setInput] = useState('');
+    function handleInput(event){
+        setInput(event.target.value)
+    }
+  return (
+    <>
+    <div className='flex'>
+        <input type="text" placeholder='Enter your task' value={inputF} onChange={handleInput}/>
+        <button className='mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700'>Add</button>
+    </div>
+    </>
+  )
+}
+
+export default InputField
