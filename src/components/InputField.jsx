ae7fee0393f05c4f52ba303f74a69c5029a6ac80
Yeabsira Mesfin <72326656+yeabsira-mesfin@users.noreply.github.com>
2024-07-23 07:14:34 -0400

    Task list.

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -2,25 +2,43 @@ import { useState } from "react";
 
 const InputField = ({ arr }) => {
   const [inputF, setInput] = useState("");
+  const [inputA,setInputArray] = useState(arr)
   function handleInput(event) {
     setInput(event.target.value);
+    
     console.log(inputF);
+    
   }
-  function handleClick() {}
+  function handleClick() {
+    setInputArray([inputF,...inputA])
+    console.log(inputA)
+  }
   return (
     <>
-      <div className="flex">
-        <input
-          type="text"
-          placeholder="Enter your task"
-          value={inputF}
-          onChange={handleInput}
-          className="mx-3 mt-4 py-2 px-4 border border-blue-500 focus:border-blue-500 
-          focus:ring-2 focus:ring-blue-500 focus:outline-none rounded"
-        />
-        <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700">
-          Add
-        </button>
+      <div className="block mx-3 mt-4">
+        <div className="flex">
+          <input
+            type="text"
+            placeholder="Enter your task"
+            value={inputF}
+            onChange={handleInput}
+            className="py-2 px-4 border border-blue-500 focus:border-blue-500 
+            focus:ring-2 focus:ring-blue-500 focus:outline-none rounded"
+          />
+          <button
+            onClick={handleClick}
+            className="bg-blue-500 text-white py-2 px-4 ml-2 rounded hover:bg-blue-700"
+          >
+            Add
+          </button>
+        </div>
+        <div className="mt-4">
+          <ul>
+            {inputA.map((input, index) => (
+              <li key={index}>{input} <button>Click</button></li>
+            ))}
+          </ul>
+        </div>
       </div>
     </>
   );
